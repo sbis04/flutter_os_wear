9d2dae4f73652a8b9b21672bd8171e7c956b3604
Souvik Biswas <sbis1999@gmail.com>
2020-01-16 23:26:59 +0530

    Removed plugin, fixed all errors

--- lib/screens/name_screen.dart
+++ lib/screens/name_screen.dart
@@ -1,27 +1,7 @@
-// Copyright (c) 2019 Souvik Biswas
-
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
 import 'package:flutter/material.dart';
-import 'package:flutter_os/screens/ambient_screen.dart';
-import 'package:flutter_os/screens/relax_menu.dart';
-import 'package:wear/wear.dart';
+import 'package:flutter_os_wear/screens/ambient_screen.dart';
+import 'package:flutter_os_wear/screens/relax_menu.dart';
+import 'package:flutter_os_wear/wear.dart';
 
 class NameScreen extends StatelessWidget {
   final screenHeight;
@@ -56,16 +36,23 @@ class NameScreenUI extends StatelessWidget {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Image.asset('assets/outline_arrow.png',scale: 1.8,),
+                    Image.asset(
+                      'assets/outline_arrow.png',
+                      scale: 1.8,
+                    ),
                     SizedBox(width: 5),
-                    Text('Back', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w300),)
+                    Text(
+                      'Back',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
+                    )
                   ],
                 ),
                 onTap: () {
                   Navigator.of(context).pop();
                 },
               ),
-              SizedBox(height: 20),
+              SizedBox(height: 15),
               Text(
                 'Welcome to',
                 style: TextStyle(
@@ -86,10 +73,10 @@ class NameScreenUI extends StatelessWidget {
                 elevation: 6.0,
                 child: Text(
                   'NEXT',
-                  style: TextStyle(color: Colors.white, fontSize: 20),
+                  style: TextStyle(color: Colors.white, fontSize: 16),
                 ),
                 shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
+                  borderRadius: BorderRadius.circular(20),
                 ),
                 color: Colors.blue[400],
                 onPressed: () {
--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -1,27 +1,7 @@
-// Copyright (c) 2019 Souvik Biswas
-
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
 import 'package:flutter/material.dart';
-import 'package:flutter_os/screens/name_screen.dart';
-import 'package:flutter_os/utils.dart';
-import 'package:wear/wear.dart';
+import 'package:flutter_os_wear/screens/name_screen.dart';
+import 'package:flutter_os_wear/utils.dart';
+import 'package:flutter_os_wear/wear.dart';
 
 class StartScreen extends StatelessWidget {
   @override
@@ -50,22 +30,24 @@ class StartScreen extends StatelessWidget {
                 mainAxisSize: MainAxisSize.max,
                 children: <Widget>[
                   FlutterLogo(size: 90),
-                  SizedBox(height: 20),
+                  SizedBox(height: 10),
                   RaisedButton(
                     highlightColor: Colors.blue[900],
                     elevation: 6.0,
                     child: Text(
                       'START',
-                      style: TextStyle(color: Colors.white, fontSize: 20),
+                      style: TextStyle(color: Colors.white, fontSize: 16),
                     ),
                     shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+                      borderRadius: BorderRadius.circular(20),
                     ),
                     color: Colors.blue[400],
                     onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                        return NameScreen(screenHeight, screenWidth);
-                      }));
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) {
+                          return NameScreen(screenHeight, screenWidth);
+                        }),
+                      );
                     },
                   )
                 ],
